edd218f6a703f8e778399fd83aa60806022a706e
sofia <anniles_@hotmail.com>
2016-10-21 15:46:27 +0300

    various improvments
    * create html file for stars and filter sidebar
    * create filter to get xs from a number eg: 5 | rangestar returns "xxxxx"
    * various css improvments

--- hotels/templatetags/hotels.py
+++ hotels/templatetags/hotels.py
@@ -5,6 +5,12 @@ register = template.Library()
 
 @register.inclusion_tag('hotels/hotel_snippet.html')
 def render_three_random_hotels():
-	hotels = Hotel.objects.all()[:3]
-	return {'hotels': hotels}
+    hotels = Hotel.objects.all()[:3]
+    return {'hotels': hotels}
 
+@register.filter
+def rangestar(value):
+    xs = ""
+    for x in range(0, abs(value)):
+        xs+="x"
+    return xs

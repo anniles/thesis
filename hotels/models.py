4574193791e77f29d7cdbe66ae669ab22d75fc48
sofia <anniles_@hotmail.com>
2016-11-01 13:27:43 +0200

    updated roomtype and amenities
    * delete old migrations
    * create migrations anew

--- hotels/models.py
+++ hotels/models.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 
 
 # Create your models here.
+class Amenity(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Hotel(models.Model):
-	CATEGORY_CHOICES = (
+    CATEGORY_CHOICES = (
         (1, '1 star'),
         (2, '2 star'),
         (3, '3 star'),
@@ -12,37 +19,62 @@ class Hotel(models.Model):
         (5, '5 star'),
     )
 
-	slug = models.SlugField(max_length=40)
-	name = models.CharField(max_length=200)
-	address = models.CharField(max_length=200)
-	phone = models.CharField(max_length=20)
-	category = models.SmallIntegerField(
+    slug = models.SlugField(max_length=40)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    category = models.SmallIntegerField(
         choices=CATEGORY_CHOICES,
         default=1,
     )
-	description = models.TextField(default='' , null=True)
+    description = models.TextField(default='' , null=True)
 
-	def __str__(self):
-		return self.name
-
-
-class RoomType(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    
     def __str__(self):
         return self.name
 
 
+class RoomType(models.Model):
+
+    TYPE_CHOICES = (
+        ('RO', 'Room only'),
+        ('BB', 'Bed & Breakfast'),
+        ('HB', 'Half Board (Breakfast and Dinner)'),
+        ('FB', 'Full Board (Beakfast, Lunch and Dinner)'),
+        ('AI', 'All Inclusive'),
+    )
+
+    roomtype = models.CharField(
+        choices=TYPE_CHOICES,
+        default='RO',
+        max_length=2,
+    )
+    coefficient = models.DecimalField(max_digits=10, decimal_places=2, default=1.0)
+
+    def __str__(self):
+        return self.roomtype
+
+
 class Room(models.Model):
+    CATEGORY_CHOICES = (
+        ('single', 'Single'),
+        ('double', 'Double'),
+    )
+
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
     roomtype = models.ForeignKey(RoomType, on_delete=models.CASCADE)
     max_guest = models.PositiveSmallIntegerField()
-    title = models.CharField(max_length=200)
-    checkin = models.DateField(null=True)
-    checkout = models.DateField(null=True)
+    category = models.CharField(
+        choices=CATEGORY_CHOICES,
+        default='single',
+        max_length=200,
+    )
     # image = models.ImageField(upload_to = 'ulpoads', default = 'static/tourguide/images/pic_folder/None/no-img.jpg' , null=True)
     description = models.TextField(default='' , null=True)
+    amenities = models.ManyToManyField(Amenity)
+    # price to do
 
     def __str__(self):
-    	return self.title
+        return self.category
+    
+    # checkin = models.DateField(null=True)
+    # checkout = models.DateField(null=True)

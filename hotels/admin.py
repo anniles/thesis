4b4e5f530466fb8b8fa9854781cf9c77c0898f71
sofia <anniles_@hotmail.com>
2016-11-01 21:31:37 +0200

    admin site customize room inside hotel page

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Hotel
+from .models import Hotel, RoomType, Room, Amenity
+
+
+class RoomInline(admin.StackedInline):
+    model = Room
+    extra = 1
+
+
+class RoomTypeInline(admin.TabularInline):
+    model = RoomType
+
 
 
 class HotelAdmin(admin.ModelAdmin):
-	#slug is the name of the hotel that appears in the url hyphen(/) seperated and lowercase
-	prepopulated_fields = {"slug": ("name",) }
+    #slug is the name of the hotel that appears in the url hyphen(/) seperated and lowercase
+    prepopulated_fields = {"slug": ("name",) }
+    inlines = [RoomInline]
 
 
 # Register your models here.
 admin.site.register(Hotel, HotelAdmin)
+# admin.site.register(Room)
+admin.site.register(RoomType)
+admin.site.register(Amenity)
